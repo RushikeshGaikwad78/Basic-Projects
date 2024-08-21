5fb77e4c272c9fc0e94e2133782f1909b454b880
RushikeshGaikwad78 <gaikwadrushi7898@gmail.com>
2024-08-21 19:45:38 +0530

    complete project

--- REST API's/index.js
+++ REST API's/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
 
 //routes
 
@@ -16,6 +19,28 @@ app.get("/users/:id", (req, res) => {
   const id = Number(req.params.id);
   const user = users.find((user) => user.id === id);
   return res.json(user);
+});
+
+//post request
+app.post("/users", (req, res) => {
+  //todo : create a new user
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "success",id:users.length });
+  });
+});
+
+//patch request
+app.patch("/users/:id", (req, res) => {
+  //todo : update the user of given id
+  return res.json({ status: "pending" });
+});
+
+//delete request
+app.delete("/users:id", (req, res) => {
+  //todo : delete the user with given id
+  return res.json({ status: "pending" });
 });
 
 app.listen(8000, () => {
