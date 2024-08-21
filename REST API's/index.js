83ad9d8704a3c6fb733ee5ea521493bf2e8bd664
RushikeshGaikwad78 <gaikwadrushi7898@gmail.com>
2024-08-21 17:43:29 +0530

    get requests done

--- REST API's/index.js
+++ REST API's/index.js
@@ -1,9 +1,23 @@
-const express = require('express');
+const express = require("express");
+const users = require("./MOCK_DATA.json");
 
 const app = express();
 
 //routes
 
-app.listen(8000,()=>{
-    console.log("App listening on port 8000!");
-})
+//getting all users
+app.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+//dynamic path parameters
+//:id -> dynamic parameter
+app.get("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  return res.json(user);
+});
+
+app.listen(8000, () => {
+  console.log("App listening on port 8000!");
+});
